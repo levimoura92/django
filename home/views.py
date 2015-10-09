610f86581d240d64fa310bfb8c8001fe13d5a065
levi <levidasilvamoura@gmail.com>
2015-10-09 12:28:01 -0300

    newsletter

--- home/views.py
+++ home/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .forms import Newsletter
 
 def home(request):
 	
+	form = Newsletter(request.POST or None)
+	
+	if form.is_valid():
+		instance = form.save(commit = False)
+		print instance.email
+		instance.save()
+	
+	context = {
+	#	"title": title,
+		"form":form,
+	}
 	return render(request, 'home.html', {})
